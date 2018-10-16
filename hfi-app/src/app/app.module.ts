8457230696aaa645404489609ada9708c6f97635
Simon Eisner <simon.eisner@geosoft.com>
2018-10-16 15:14:04 -0400

    created player and player list components

--- hfi-app/src/app/app.module.ts
+++ hfi-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayerListComponent } from './components/player-list/player-list.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerListComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
